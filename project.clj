84589354e3ea925616063b0cd77ed2a7a475290a
Anders Eliasson <anders.eliasson@digitalroute.com>
2017-11-02 07:50:37 +0100

    first commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject bchain "0.1.0-SNAPSHOT"
+  :description "blockchain.info API"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.7.0"]
+                 [org.clojure/data.json "0.2.6"]])
---
+++ src/bchain/core.clj
@@ -0,0 +1,86 @@
+(ns bchain.core
+  "Functions using the blockchain.info API"
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]))
+
+(defmacro dbg [body]
+  `(let [x# ~body]
+     (println "dbg:" '~body "=" x#)
+     x#))
+
+(defn blockchain-call [arg]
+  (let [res (client/get (format "https://blockchain.info/%s" arg))]
+    (if (= (:status res) 200) 
+      (-> res :body json/read-str)
+      (throw (IllegalStateException. (pr-str res))))))
+
+
+(defn exchange-rates [] (blockchain-call "ticker"))
+
+(defn dollar-rate [] (((exchange-rates) "USD") "last"))
+
+(defn tobtc [value to-currency]
+  (blockchain-call (format "tobtc?currency=%s&value=%s" to-currency value)))
+
+(defn blockchain-query-call [arg] (blockchain-call (format "q/%s" arg)))
+
+(defmacro def-query [& args]
+  `(do 
+     ~@(map (fn [a] `(defn ~a [] (blockchain-query-call ~(str a)))) args)))
+
+
+;realtime
+(def-query 
+  getdifficulty 
+  getblockcount 
+  latesthash
+  bcperblock
+  totalbc
+  probability
+  hashestowin
+  nextretarget
+  avgtxsize 
+  avgtxvalue
+  interval 
+  eta
+  avgtxnumber)
+
+
+;misc
+
+(def-query 
+  unconfirmedcount
+  marketcap
+  hashrate
+  rejected)
+  
+  
+
+
+(defn _24hrprice [] (blockchain-query-call "24hrprice"))
+(defn _24hrtransactioncount  [] (blockchain-query-call "24hrtransactioncount "))
+(defn _24hrbtcsent [] (blockchain-query-call "24hrbtcsent"))
+
+
+(defn block-of [height] (blockchain-call (format "block-height/%s?format=json" height)))
+
+(defn latestblock [] (blockchain-call "latestblock"))
+
+(defn rawblock 
+  ([block-hash] (blockchain-call (format "rawblock/%s" block-hash)))
+  ([block-hash frmt] (blockchain-call (format "rawblock/%s?format=%s" block-hash frmt))))
+
+(defn rawtx [tx] (blockchain-call (format "rawtx/%s" tx)))
+(defn rawtaddr [btc-addr] (blockchain-call (format "rawtaddr/%s" btc-addr)))
+
+(defn blocks 
+  ([] (blocks 0 (getblockcount) ))
+  ([i n] (if (< i n) 
+           (lazy-seq (cons (block-of i) (blocks (inc i) n)))
+           '())))
+           
+(defn transactions []
+  (mapcat #(% "blocks") (blocks))
+  )
+  
+
