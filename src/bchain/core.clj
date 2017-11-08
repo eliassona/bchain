e4a332cf14557d93f333fa6dece47dda4f2dd8ad
Anders Eliasson <anders.eliasson@digitalroute.com>
2017-11-08 12:11:20 +0100

    more fns

--- src/bchain/core.clj
+++ src/bchain/core.clj
@@ -9,20 +9,40 @@
      (println "dbg:" '~body "=" x#)
      x#))
 
+
+(defn- delim-str-of 
+  ([d l]
+  (reduce (fn [acc v]
+          (if acc 
+            (format "%s%s%s" acc d v)
+            v)) l))
+  ([l]
+  (delim-str-of "|" l)))
+
+(defn- reduce-args [args] 
+  (reduce 
+    (fn [acc [k v]]
+      (format
+        "%s%s=%s"
+        (if acc 
+          (format "%s&" acc)
+          "") k v)) 
+    nil 
+    (partition 2 args)))
+
 (def satoshi 0.00000001)
 
-(defn http-call [addr arg]
+(defn- http-call [addr arg]
   (let [res (client/get (format "%s/%s" addr arg))]
     (if (= (:status res) 200) 
       (-> res :body json/read-str)
       (throw (IllegalStateException. (pr-str res))))))
 
-(defn blockchain-call [arg] (http-call "https://blockchain.info" arg))
+(defn- blockchain-call [arg] (http-call "https://blockchain.info" arg))
 
-(defn blockchain-api-call [arg] (http-call "https://api.blockchain.info" arg))
+(defn- blockchain-api-call [arg] (http-call "https://api.blockchain.info" arg))
 
 (defn ticker [] (blockchain-call "ticker"))
-
 
 (defn tobtc [value to-currency]
   (blockchain-call (format "tobtc?currency=%s&value=%s" to-currency value)))
@@ -51,6 +71,46 @@
   avgtxnumber)
 
 
+;address lookups
+
+(defmacro def-address-lookup [& args]
+  `(do
+     ~@(map 
+         (fn [a] 
+           `(defn ~a [confirmations# & addresses#] 
+              (blockchain-query-call (format "%s/%s?confirmations=%s" ~(str a) (delim-str-of addresses#) confirmations#)))) args)))
+
+(def-address-lookup 
+  addressbalance 
+  getreceivedbyaddress 
+  getsentbyaddress
+  )
+
+(defn addressfirstseen [address] (blockchain-query-call (format "addressfirstseen/%s" address)))
+
+(defn addressbalance 
+  [confirmations & addresses]
+   (blockchain-query-call (format "addressbalance/%s?confirmations=%s" (delim-str-of addresses) confirmations)))
+
+
+;tx lookups
+
+(defn txtotalbtcoutput [tx-hash] (blockchain-query-call (format "txtotalbtcoutput/%s" tx-hash)))
+(defn txtotalbtcinput [tx-hash] (blockchain-query-call (format "txtotalbtcinput/%s" tx-hash)))
+(defn txfee [tx-hash] (blockchain-query-call (format "txfee/%s" tx-hash)))
+(defn txresult [tx-hash & addresses] (blockchain-query-call (format "txresult/%s/%s" tx-hash (delim-str-of addresses))))
+
+
+;tools
+
+(defn addresstohash [address] (blockchain-query-call (format "addresstohash/%s" address)))
+(defn hashtoaddress [hash] (blockchain-query-call (format "hashtoaddress/%s" hash)))
+(defn hashpubkey [pub-key] (blockchain-query-call (format "hashpubkey/%s" pub-key)))
+(defn addrpubkey [pub-key] (blockchain-query-call (format "addrpubkey/%s" pub-key)))
+(defn pubkeyaddr [address] (blockchain-query-call (format "pubkeyaddr/%s" address)))
+
+
+
 ;misc
 
 (def-query 
@@ -74,16 +134,9 @@
 
 (defn stats [] (blockchain-api-call "stats"))
 
-(defn- reduce-args [args] 
-  (reduce 
-    (fn [acc [k v]]
-      (format
-        "%s%s=%s"
-        (if acc 
-          (format "%s&" acc)
-          "") k v)) 
-    nil 
-    (partition 2 args)))
+ 
+
+
 
 (defn charts [type & args]
   (assert (-> args count even?))
@@ -96,12 +149,22 @@
     (blockchain-api-call "pools")))
 
 (comment 
-  Below is not part of the blockchain.info API, it provides lazy seqs to some blockchain datastructures such as blocks and transactions.
+  Below is not part of the blockchain.info API, it provides for example lazy seqs to some blockchain datastructures such as blocks and transactions.
 )
 
+(defn- rate-type-of [r t]
+  `(defn ~(symbol (format "%s-%s" r t)) [] ((~r) ~t)))
+
 (defn- rate-of [x]
-  (let [k (key x)]
-    `(defn ~(symbol k) [] ((ticker) ~k))))
+  (let [k (key x)
+        r (symbol k)]
+    `(do 
+       (defn ~r [] ((ticker) ~k))
+       ~(rate-type-of r "last")
+       ~(rate-type-of r "buy")
+       ~(rate-type-of r "sell")
+       ~(rate-type-of r "symbol")
+       )))
 
 (defmacro def-rates []
   "Define functions for all currencies, i.e (USD) for US-dollars"
@@ -114,7 +177,7 @@
 
 (defn blocks 
   "all blocks in bitcoin as a lazy seq"
-  ([] (blocks 0 (getblockcount) ))
+  ([] (blocks 0 (getblockcount)))
   ([i n] (if (< i n) 
            (lazy-seq (cons (block-height i) (blocks (inc i) n)))
            '())))
