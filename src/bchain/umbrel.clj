942ea6dda4915ec62f907bcd57a00bf87db7a56c
Anders Eliasson <anders.eliasson@digitalroute.com>
2022-04-30 09:02:24 +0200

    stuff

--- src/bchain/umbrel.clj
+++ src/bchain/umbrel.clj
@@ -1,6 +1,13 @@
 (ns bchain.umbrel
+  (:use [clojure.pprint])
+  (:require [clojure.data.json :as json])
   (:import [com.jcraft.jsch JSch]
            [java.io ByteArrayOutputStream]))
+
+(defmacro dbg [body]
+  `(let [x# ~body]
+     (println "dbg:" '~body "=" x#)
+     x#))
 
 (defn ssh [user host port password command]
   (let [session (.getSession (JSch.) user host port)
@@ -27,4 +34,32 @@
 (defn bitcoin-cli [password cmd]
   (umbrel password (format "cd ~/umbrel/bin; docker exec bitcoin bitcoin-cli %s" cmd)))
     
-    
+#_(def bitcoin-cli (partial bitcoin-cli "mypw"))    
+
+(defn format-str-of [n]
+  (reduce 
+    (fn [acc _] 
+      (if acc 
+        (str acc " %s") 
+        (str "%s"))) 
+    nil (range n)))
+
+(defmacro def-api 
+  ([cmd conv-fn args]
+    (let [pw (symbol "password")]
+      `(defn ~(symbol cmd) 
+         (~args (~conv-fn (bitcoin-cli (format ~(format-str-of (inc (count args))) ~cmd ~@args))))
+         (~(vec (concat [pw] args)) (~conv-fn (bitcoin-cli ~pw (format ~(format-str-of (inc (count args))) ~cmd ~@args)))))))
+  ([cmd conv-fn]
+    `(def-api ~cmd ~conv-fn []))
+  ([cmd]
+    `(def-api ~cmd identity))
+  )
+  
+(def-api "getbestblockhash" identity)
+(def-api "getblockhash" identity [index])
+(def-api "getblock" identity [blockhash verbosity])
+(def-api "getblockchaininfo" json/read-str)
+(def-api "getblockcount" read-string)
+(def-api "getblockfilter" identity [blockhash verbosity])
+
